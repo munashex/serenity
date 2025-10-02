42dd320d8532113bf759927cece17886d4435114
munashe <munashemugondaa@gmail.com>
2025-10-02 23:41:14 +0200

    done with first section on home page on bg screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,23 @@ import face3 from '../images/home/face3.avif'
 import image1 from '../images/home/image1.avif' 
 import image2 from '../images/home/image2.avif'
 import { MdAutorenew as Renew } from "react-icons/md";
+import { FaStarOfLife as Mental } from "react-icons/fa";
+import { VscWorkspaceTrusted as Trust } from "react-icons/vsc";
+import { FaHourglassStart } from "react-icons/fa";
+import { RiMentalHealthFill } from "react-icons/ri";
 
 const facesImages = [
     {image: face1, id: 1}, 
     {image: face2, id: 2}, 
     {image: face3, id: 3}
+]
+
+const homeData = [ 
+{name: "Mental Wellness", icon: <Mental size={20}/>}, 
+{name: "Built on Trust", icon: <Trust size={20}/>},
+{name: "Start Your Healing Journey", icon: <FaHourglassStart size={20}/>}, 
+{name: "Heal", icon: <RiMentalHealthFill size={20}/>}
+
 ]
 
 const Home = () => {
@@ -67,7 +79,69 @@ const Home = () => {
            </div>
           </div> 
 
+            {/* first home section on lg screens*/} 
+            <div className="hidden md:grid grid-cols-3 gap-x-6 mt-24 items-start">
+             <div className="flex flex-col gap-5">
+             <div className="flex items-center gap-5">
+            <span className="bg-[#8d6959] inline-flex items-center gap-1 rounded-full py-1 px-3 text-white">
+            <FaStar color="gold" size={18}/> 4.7 Rated
+            </span> 
+            <h1 className="text-[#8d6969]">By Satisfied customers</h1>
+           </div>
+           
+           <h1 className="text-5xl font-bold  max-w-xl">You Deserve to Feel Better. Peace Begins with Us</h1>
+           <h1 className="text-[#8d6969] max-w-lg">We offer compassionate and personalized mental health support to guide life’s challenges.</h1>
+          <Link to="/appointment" className="default-btn w-fit">Book an appointment</Link>
           
+           <div className="flex mt-8">
+            {facesImages.map((face) => (
+            <div key={face.id}  className={`-ml-4 first:ml-0 rounded-full border-2 border-white`}>
+             <img src={face.image} alt={`serenity-face${face.id}`} 
+             className="w-14"
+             />
+             </div>
+            ))}
+           </div>
+
+           <h1 className="text-[#8d6969]"><span className="text-[#e39a86] font-bold">300+</span>  Individuals who have trusted Serenity Minds</h1>
+             </div>
+
+             <div className="relative">
+           <img src={image1} alt="serenity-1"  
+            className="object-cover rounded-xl"
+           />
+          <div className="absolute top-20 right-7 inline-flex items-center">
+          <div className="bg-[#8d6959] w-8 h-8 rounded-full p-1.5">
+            <div className="h-5 w-5 rounded-full bg-[#d5c7c0] animate-pulse"></div>
+            </div>
+          <h1 className="bg-[#8d6959] py-1 px-3 text-white -ml-1 rounded-full">Healthy Mind</h1>
+          </div>
+
+          <div className="absolute bottom-5 left-7 flex flex-wrap gap-4">
+            {homeData.map((item) => (
+            <div key={item.name} className="bg-white  py-2 px-3 rounded-full inline-flex items-center gap-2">  
+              <span className="text-[#e1937d]">{item.icon}</span>
+              <h1 className="text-[#997a6b] font-bold">{item.name}</h1>
+            </div>
+            ))}
+          </div>
+           </div>
+
+           <div className="flex flex-col gap-y-12">
+            <div className="space-y-4">
+            <h1 className="font-bold text-3xl">You Deserve Peace</h1>
+            <h1 className="text-[#8d6969] max-w-sm">We offer compassionate, personalized mental health support.</h1>
+            </div>
+            <div className="relative">
+           <img src={image2} alt="serenity-1"  
+            className="object-cover rounded-xl h-96  w-full"
+           />
+           <h1 className="absolute top-10 right-16 text-lg bg-black/20 text-white inline-flex items-center gap-1 py-1 px-3 rounded-full font-bold">
+            <Renew color="#8d6959" size={25}/> Renew
+            </h1>
+           </div>
+           </div>
+            </div>
 
         </div>
     )
